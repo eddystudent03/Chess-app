b7e1a917639adef8a736056863851129ce4578c6
Eddy Mulisho <eddy.mulisho@student.hogent.be>
2024-04-01 14:37:38 +0200

    ik ga verder in java

--- chess/chess app/src/components/Board.jsx
+++
@@ -1,7 +0,0 @@
-import {useState} from 'react'
-import Piece from './Piece'
-function Board(){
-    const chessboard = Array(8).fill().map(() => Array(8).fill(null));
-    const [board, setBoard] = (chessboard)
-
-}
